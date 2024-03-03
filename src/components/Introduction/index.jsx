0f25c21aefe5e820d48c6d098e0790a6ed62b856
Poliana <polianariveiro@gmail.com>
2024-03-03 01:34:28 -0300

    ajustando o css

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -1,28 +1,25 @@
 import styles from './Introduction.module.css';
-import { BiLogoGmail } from "react-icons/bi";
+import { SiGmail } from "react-icons/si";
 import { VscGithub } from "react-icons/vsc";
 import { ImLinkedin2 } from "react-icons/im";
 
 export default function Introduction() {
-
-return (
-
-<section className={styles.section}>
-
-<img src="https://images.pexels.com/photos/6447547/pexels-photo-6447547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Poliana Ribeiro - perfil"/>
-
-<h2>Olá, mundo! Meu nome é Poliana!</h2>
-
-<a href="https://github.com/polianarm" target="_blank" rel="noopener noreferrer"> <VscGithub size={28}/> </a>
-<a href="https://www.linkedin.com/in/polianar/" target="_blank" rel="noopener noreferrer"> <ImLinkedin2 size={28}/> </a>
-<a href="mailto:polianaaa.ribeiro@gmail.com" target="_blank" rel="noopener noreferrer"> <BiLogoGmail size={28}/> </a>
-</section>
-
-
-)
-
-
-    
-}
-
-
+    return (
+      <section className={styles.sectionIntroduction}>
+        <div className={styles.perfil}>
+          <img className={styles.perfilImg} src="https://images.pexels.com/photos/6447547/pexels-photo-6447547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Poliana Ribeiro - perfil" />
+        </div>
+  
+        <div className={styles.hello}>
+          <h2>Olá, mundo! Meu nome é Poliana!</h2>
+          <h3>Me interesso por muitas coisas</h3>
+  
+          <div className={styles.links}>
+            <a href="https://github.com/polianarm" target="_blank" rel="noopener noreferrer"> <VscGithub size={28} className={styles.links}/> </a>
+            <a href="https://www.linkedin.com/in/polianar/" target="_blank" rel="noopener noreferrer"> <ImLinkedin2 size={28} className={styles.links}/> </a>
+            <a href="mailto:polianaaa.ribeiro@gmail.com" target="_blank" rel="noopener noreferrer"> <SiGmail size={28} className={styles.links}/> </a>
+          </div>
+        </div>
+      </section>
+    );
+  }
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,13 +1,16 @@
 import styles from "./Skills.module.css";
+import { GrReactjs } from "react-icons/gr";
 
 export default function Skills() {
 
 
   return (
-    <section id="skills" className={styles.section}>
-      <h2>Skilss</h2>
+    <section id="skills" className={styles.sectionSkillss}>
+      <h2>Skillss</h2>
 
       <p>Algumas coisas que sei fazer</p>
+
+      <GrReactjs size={28} className={styles.links}/> 
     </section>
   );
 }
